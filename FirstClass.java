adf9e5f91edacdad68158a178aa6b666aa76906e
monumca <monumca2011@gmail.com>
2019-10-13 09:52:47 +0530

    Updated Java from 11 to 12

--- FirstClass.java
+++ FirstClass.java
@@ -1,6 +1,6 @@
 import java.lang.*;
 class FirstClass{
 	public static void main(String args[]){
-		System.out.println("Hello, Welcome in Java 11");
+		System.out.println("Hello, Welcome in Java 12");
 	}
 }
