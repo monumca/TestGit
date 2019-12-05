53039814e3dd64a2fb5501efe9891e6336c0ddbc
monumca <monumca2011@gmail.com>
2019-12-05 21:55:44 +0530

    Added property file

--- First.java
+++
@@ -1,6 +0,0 @@
-import java.lang.*;
-class FirstClass{
-	public static void main(String args[]){
-		System.out.println("Hello, Welcome in Java 12");
-	}
-}
--- FirstClass.java
+++ FirstClass.java
@@ -1,6 +1,6 @@
 import java.lang.*;
 class FirstClass{
 	public static void main(String args[]){
-		System.out.println("Hello, Welcome in Java 12");
+		System.out.println("Hello, Welcome in Java 11");
 	}
 }
